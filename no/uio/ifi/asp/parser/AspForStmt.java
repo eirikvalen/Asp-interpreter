d2b4e80c632058d7b7855666aead125f4c3f1c24
Eirik Valen <eiriv@uio.no>
2020-09-23 17:00:48 +0200

    Added more classes

---
+++ no/uio/ifi/asp/parser/AspForStmt.java
@@ -0,0 +1,33 @@
+package no.uio.ifi.asp.parser;
+
+import no.uio.ifi.asp.scanner.Scanner;
+import no.uio.ifi.asp.scanner.TokenKind;
+
+public class AspForStmt extends AspCompoundStmt{
+    AspName name;
+    AspExpr expression;
+    AspSuite suite;
+
+
+    AspForStmt(int n) {
+        super(n);
+    }
+
+    public static AspForStmt parse(Scanner s){
+        enterParser("For");
+
+        AspForStmt afs = new AspForStmt(s.curLineNum());
+        skip(s, TokenKind.forToken);
+        afs.name = AspName.parse(s);
+        skip(s, TokenKind.inToken);
+        afs.expression = AspExpr.parse(s);
+        skip(s, TokenKind.colonToken);
+        afs.suite = AspSuite.parse(s);
+
+        leaveParser("For");
+
+        return afs;
+    }
+
+
+}
---
+++ no/uio/ifi/asp/parser/AspNoneLiteral.java
@@ -0,0 +1,34 @@
+package no.uio.ifi.asp.parser;
+
+import no.uio.ifi.asp.runtime.RuntimeReturnValue;
+import no.uio.ifi.asp.runtime.RuntimeScope;
+import no.uio.ifi.asp.runtime.RuntimeValue;
+import no.uio.ifi.asp.scanner.Scanner;
+import no.uio.ifi.asp.scanner.TokenKind;
+
+public class AspNoneLiteral extends AspAtom {
+    AspNoneLiteral(int n) {
+        super(n);
+    }
+
+    public static AspNoneLiteral parse(Scanner s){
+        enterParser("None literal");
+        AspNoneLiteral anl = new AspNoneLiteral(s.curLineNum());
+        skip(s, TokenKind.noneToken);
+        leaveParser("None literal");
+        return anl;
+
+
+
+    }
+
+    @Override
+    void prettyPrint() {
+
+    }
+
+    @Override
+    RuntimeValue eval(RuntimeScope curScope) throws RuntimeReturnValue {
+        return null;
+    }
+}
